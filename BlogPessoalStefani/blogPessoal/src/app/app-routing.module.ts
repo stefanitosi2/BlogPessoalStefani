69093ec37fc324725712abef012bf33d61913868
stefanitosi2 <stefanitosi2@hotmail.com>
2021-09-27 14:00:24 -0300

    Implementação da rota

--- BlogPessoalStefani/blogPessoal/src/app/app-routing.module.ts
+++ BlogPessoalStefani/blogPessoal/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EntrarComponent} from './entrar/entrar.component'; 
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'cadastrar', component: CadastrarComponent},
+  {path:'entrar', component: EntrarComponent},
+  {path:'',redirectTo:'entrar', pathMatch:'full'}
+
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
