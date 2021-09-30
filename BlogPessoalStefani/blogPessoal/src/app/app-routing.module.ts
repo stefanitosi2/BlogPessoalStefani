c7ffd8236f7e815237289cb14eea085287b7784f
stefanitosi2 <stefanitosi2@hotmail.com>
2021-09-30 17:27:57 -0300

    Implementação do componente tema

--- BlogPessoalStefani/blogPessoal/src/app/app-routing.module.ts
+++ BlogPessoalStefani/blogPessoal/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {EntrarComponent} from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 const routes: Routes = [
@@ -11,7 +12,9 @@ const routes: Routes = [
   {path:'cadastrar', component: CadastrarComponent},
   {path:'entrar', component: EntrarComponent},
 
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+
+  {path: 'tema', component: TemaComponent}
 
 
 
