1129671f2f9db35c6792c7878783d854a5757527
stefanitosi2 <stefanitosi2@hotmail.com>
2021-09-30 16:17:53 -0300

    Componente inicio sem consumo

--- BlogPessoalStefani/blogPessoal/src/app/service/auth.service.ts
+++ BlogPessoalStefani/blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { user } from '../model/user';
 import { userLogin } from '../model/userLogin';
 
@@ -19,7 +20,15 @@ export class AuthService {
 
   cadastrar(user: user): Observable <user>{
     return this.http.post<user>('https://backendblogpessoal.herokuapp.com/usuarios/cadastrar', user)
+  }
 
+  logado(){
+    let ok: boolean = false
 
+    if (environment.token != ''){
+      ok = true
+
+    }
+    return ok
   }
 }
